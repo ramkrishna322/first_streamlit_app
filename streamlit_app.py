ed42a5a3bd94db099f96a4dc6cdfd56dd3c12254
ramkrishna322 <107311871+ramkrishna322@users.noreply.github.com>
2022-06-12 15:23:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭Build Your Own Fruit Smoothie 🥝🍇')
-fruityvice_response=requests.get("htts://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list=my_fruit_list.set_index('Fruit')
@@ -21,5 +20,8 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response=requests.get("htts://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
