b6aca6b3dfbbcb443e315b4f4fd294ab432a680c
ramkrishna322 <107311871+ramkrishna322@users.noreply.github.com>
2022-06-12 21:47:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
+
+my_cnx=snowflake.connector.connect(**streamlit.secret["snoflake"])
+my_cur=my_cnx.cursor()
+my_cur.execute("select current_user(),current_account(),current_region()")
+my_data_row=my_cur.fetchone()
+streamlit.text("Hello from snowflake:")
+streamlit.text(my_data_row)
+
